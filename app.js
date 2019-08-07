6042f545ff6a0564ef887cbfbc04a4ea58d4d005
Alexander Hoinville <ahoin001@gmail.com>
2019-08-07 19:24:27 -0400

    Change functions to use model file and schema

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+// Allows us too use model created in other file
+const theCatModel = require('./models/Cat.js')
+
 // Same instance of mongoose is shared across the application, after first using require mongoose, any require mongoose 
 // in other files will be talking to the database determined here
 
@@ -9,39 +12,27 @@ mongoose
     .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
     .catch(err => console.error('Error connecting to mongo', err));
 
-// TODO: Understand Schemas better 
-// We are creating a model called Cat in first parameter, That creates documents with the blueprint passed in the second parameter is a (schema)
-const Cat = mongoose.model('Cat', { name: String });
-
 // function that will recive a name and create then add new cat instance too DB
 const addNewCat = (catName) => {
 
-    // create a new instance of cat that we will later add to database
-    const kitten = new Cat({ name: catName });
-
-    // since kitty is based off of Mongoose Model "Cat", it has acccess to mongoose model methods
-    // Save() sends object to our mongoDB using insertOne command behind the scenes
-    kitten
-        .save()
-        .then(newCat => console.log(`New cat ${catName} saved.`))
-        .catch(err => console.log(`Error while trying to save new Cat ${err}`))
+    // Add new document using schema we created in model
+    theCatModel.create({ name: catName, color: "black", age: 5 })
+        .then(cat => console.log(`Added ${cat} to the database`))
+        .catch(err => { console.log(`Error when adding cat`) });
 
 }
 
 const listTheCats = () => {
 
-    Cat
-        .find()
-        .then(catsFromDb => {
-            // with returned array of cats, we can loop through to print them
-            catsFromDb.forEach(currentCat => {
-                console.log(`--> Cat: ${currentCat.name}`);
-            });
+    // Promise version
+    theCatModel.find({})
+        .then((cats) => {
+            console.log(`Cat List Returned`,cats);
         })
-        .catch(err => {
-            console.log(`Error returning Cat.find : ${err}`)
+        .catch((err) => {
+            console.log(`Error returning list`);
         }
-        )
+        );
 
 }
 
@@ -54,7 +45,11 @@ const addTenCats = () => {
 }
 
 const deleteAllCatsNamedAlex = () => {
-    Cat.deleteMany({ name: "Alex" });
+    // Delete all the users that have "@ironhack.com" in their email
+    theCatModel.deleteMany({ name: "Alex" })
+        .then(cats => console.log(`All cats named Alex deleted`, cats))
+        .catch(err => console.log(`Error deleting the Alex cats`, err));
+
 }
 
 const deleteCatWithThisName = (targetCatName) => {
@@ -62,6 +57,7 @@ const deleteCatWithThisName = (targetCatName) => {
 }
 
 // listTheCats();
-//deleteAllCatsNamedAlex();
 // addTenCats();
-
+//listTheCats();
+//addNewCat("Alex");
+deleteAllCatsNamedAlex();
--- models/Cat.js
+++ models/Cat.js
@@ -1,16 +1,18 @@
-
+// It knows which mongoose to connnect to since we defined a connection in another file
 const mongoose = require('mongoose');
 
 // Define a schema
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 // Create new instance of Schema to be used in collection
 const catSchema = new Schema({
-  name : String,
-  color: String,
-  age  : Number
+    name: String,
+    color: String,
+    age: Number
 });
 
-// Create new collection that uses our schema
+// Create new collection(model) that uses our schema
 const Cat = mongoose.model('Cat', catSchema);
+
+// Allows Cat model to be used in other files that require it
 module.exports = Cat;
